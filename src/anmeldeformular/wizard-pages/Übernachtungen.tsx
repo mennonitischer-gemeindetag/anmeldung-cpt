22e6b7de461a4dccc8a59c1577fb73c5deaecd81
Fabian Kägy <mail@fabian-kaegy.de>
2023-01-10 07:47:22 +0100

    update sleeping arrangement

--- src/anmeldeformular/wizard-pages/Übernachtungen.tsx
+++ src/anmeldeformular/wizard-pages/Übernachtungen.tsx
@@ -10,10 +10,9 @@ export default function Uebernachtungen() {
 		<Wizard.Page>
 			<h2 className={ 'section-heading' }>Übernachtung</h2>
 			<p>
-				Unterkünfte bitte selbständig buchen. Für Jugendliche,
-				Studierende, Azubis und FSJler*innen steht eine
-				Gruppenunterkunft in der Turnhalle des Gymnasium Weierhof zur
-				Verfügung (Lageplan Nr. 7).
+				Hotels bitte selbständig buchen. Für Jugendliche, und junge
+				Erwachsene steht eine Gruppenunterkunft in einem Massenlager zur
+				Verfügung. Der Ort wird noch bekannt gegeben.
 			</p>
 			<div>
 				<Field
@@ -23,57 +22,14 @@ export default function Uebernachtungen() {
 					id={ 'uebernachtung_and_breakfast' }
 				/>
 				<label htmlFor={ 'uebernachtung_and_breakfast' }>
-					Jugend Übernachtung mit Frühstück - 15€
+					Massenlager inkl. Frühstück von Freitag (28.04.2023) bis
+					Montag (01.05.2023)
 				</label>
 			</div>
-			{ hasUebernachtungAndBreakfast && (
-				<section>
-					<div>
-						<Field
-							component={ 'input' }
-							name={ 'uebernachtung' }
-							value={ 'Turnhalle' }
-							type={ 'radio' }
-							id={ 'uebernachtung-turnhalle' }
-						/>
-						<label htmlFor={ 'uebernachtung-turnhalle' }>
-							Übernachtung in der Turnhalle des Gymnasium Weierhof
-							mit Frühstück (08:00 - 9:00 Uhr) von Donnerstag
-							(21.05.2020) bis Sonntag (24.05.2020)
-						</label>
-					</div>
-					<div>
-						<Field
-							component={ 'input' }
-							name={ 'uebernachtung' }
-							value={ 'Eigenes Zelt' }
-							type={ 'radio' }
-							id={ 'uebernachtung-eigenes-zelt' }
-						/>
-						<label htmlFor={ 'uebernachtung-eigenes-zelt' }>
-							Ich bringe mein eigenes Zelt mit
-						</label>
-					</div>
-					<div>
-						<Field
-							component={ 'input' }
-							name={ 'uebernachtung' }
-							value={ 'Im Zelt mit...' }
-							type={ 'radio' }
-							id={ 'uebernachtung-zelt-mit-checkbox' }
-						/>
-						<label htmlFor={ 'uebernachtung-zelt-mit-checkbox' }>
-							Übernachtung im Zelt mit ...
-						</label>
-						<Field
-							component={ 'input' }
-							name={ 'uebernachtung_zelt_mit' }
-							type={ 'text' }
-							id={ 'uebernachtung-zelt-mit' }
-						/>
-					</div>
-				</section>
-			) }
+			<p>
+				Anmeldungen für die Jugendherberge Leutesdorf erfolgen auf einer
+				extra Anmeldeseite
+			</p>
 		</Wizard.Page>
 	);
 }
