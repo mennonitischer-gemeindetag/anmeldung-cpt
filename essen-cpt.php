7cfae25b2eb3f7295d2bf36d0c1ae115661f4820
Fabian Kägy <mail@fabian-kaegy.de>
2023-01-10 07:46:09 +0100

    fix remove unused vars

--- essen-cpt.php
+++ essen-cpt.php
@@ -59,21 +59,6 @@ $string           = [
 	'single'       => true,
 	'show_in_rest' => true,
 ];
-$boolean          = [
-	'type'         => 'boolean',
-	'single'       => true,
-	'show_in_rest' => true,
-];
-$number           = [
-	'type'         => 'number',
-	'single'       => true,
-	'show_in_rest' => true,
-];
-$multiple_numbers = [
-	'type'         => 'number',
-	'single'       => false,
-	'show_in_rest' => true,
-];
 
 register_post_meta( 'essen', 'price', $string );
 register_post_meta( 'essen', 'tag', $string );
